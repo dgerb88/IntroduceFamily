437b3f9205817f83966a24c85157222dcfd5e929
Dax Gerber <dax.gerber@gmail.com>
2023-10-27 10:20:17 -0600

    fixes for parker

---
+++ Introduce Familia/Family Member.swift
@@ -0,0 +1,14 @@
+//
+//  Family Member.swift
+//  Introduce Familia
+//
+//  Created by Dax Gerber on 10/27/23.
+//
+
+import Foundation
+
+struct familyMember {
+    var name: String
+    var image: String
+    var description: String
+}
--- Introduce Familia/TableViewController.swift
+++ Introduce Familia/TableViewController.swift
@@ -9,7 +9,25 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-    var listOfFamilyMembers = ["dad", "mom", "koen", "navy"]
+    
+    var listOfFamilyMembers = 
+    [
+    familyMember(name: "Dad",
+                image: "dax",
+                description: "Dax currently works as a physical therapist, but is interested in a change of career into IOS development. In his spare time, he enjoys playing sports, 3D printing things, and doing projects around the house."),
+    familyMember(name: "Mom",
+                 image: "kylie",
+                 description: "Kylie is the beautiful mother of 2 kiddos. She works as a nurse PRN but primarily stays home with the kids. She enjoys designing things, getting out of the house to do something fun, and chocolate in any form"),
+    familyMember(name: "Navy",
+                 image: "navy",
+                 description: "Navy is 1 1/2 years old. She likes snacks, is a complete daredevil, and is a mama's girl through and through."),
+    familyMember(name: "Koen",
+                 image: "koen",
+                 description: "Koen is 3 1/2 years old. He enjoys playing with cars, building blocks, and fixing things. He has also decided that he would like to be spider-man for halloween this year.")
+    ]
+    
+    
+    
     var indexRow = 0
 
     override func viewDidLoad() {
@@ -39,7 +57,7 @@ class TableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "member", for: indexPath)
 
         var content = cell.defaultContentConfiguration()
-        content.text = listOfFamilyMembers[indexPath.row]
+        content.text = listOfFamilyMembers[indexPath.row].name
         cell.contentConfiguration = content
         // Configure the cell...
 
@@ -56,16 +74,7 @@ class TableViewController: UITableViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destination = segue.destination as! SecondViewController
-        
-        if listOfFamilyMembers[indexRow] == "mom" {
-            destination.nameOfPerson = "Kylie"
-        } else if listOfFamilyMembers[indexRow] == "dad" {
-            destination.nameOfPerson = "Dax"
-        } else if listOfFamilyMembers[indexRow] == "koen" {
-            destination.nameOfPerson = "Koen"
-        } else if listOfFamilyMembers[indexRow] == "navy" {
-            destination.nameOfPerson = "Navy"
-        }
+        destination.familyMember = listOfFamilyMembers[indexRow]
     }
     
     /*
